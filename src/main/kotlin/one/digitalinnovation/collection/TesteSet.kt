28dd73ff2d4d18bb921f0043469ba78ce0595168
daniloescobar <daniloescobar2006@yahoo.com.br>
2022-07-29 14:55:34 -0300

    Map, Set e Mutables

--- src/main/kotlin/one/digitalinnovation/collection/TesteSet.kt
+++ src/main/kotlin/one/digitalinnovation/collection/TesteSet.kt
@@ -4,9 +4,10 @@ fun main() {
     val joao = Funcionario("Joao", 2000.0, "CLT")
     val pedro = Funcionario("Pedro", 1500.0, "CLT")
     val maria = Funcionario("Maria", 4000.0, "PJ")
+    val danilo = Funcionario("Danilo", 40000.0, "PJ")
 
     val funcionarios1 = setOf(joao, pedro)
-    val funcionarios2 = setOf(maria)
+    val funcionarios2 = setOf(maria, danilo)
 
     val resultUnion = funcionarios1.union(funcionarios2)
     resultUnion.forEach { println(it) }
